be8292125093d1719be40b12a8dd73fdeff4ed5a
vinayakushakola <vinayak.ushakola@gmail.com>
2020-06-22 09:05:01 +0530

    Added: Admin-Registration Request & Response Model

---
+++ BookStoreCommonLayer/RequestModels/AdminRequestModels.cs
@@ -0,0 +1,29 @@
+﻿//
+// Author   : Vinayak Ushakola
+// Date     : 21 June 2020
+// Purpose  : It Contain Models that Admin may Requests
+//
+
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreCommonLayer.RequestModels
+{
+    public class AdminRegistrationRequest
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Your FirstName Length Should be more than 3")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Your LastName Length Should be more than 3")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Please Enter a Proper Email-ID")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Your Password Should be Minimum Length of 5")]
+        public string Password { get; set; }
+    }
+}
---
+++ BookStoreCommonLayer/ResponseModels/AdminResponseModels.cs
@@ -0,0 +1,29 @@
+﻿//
+// Author   : Vinayak Ushakola
+// Date     : 21 June 2020
+// Purpose  : It Contain Admin Response Models
+//
+
+using System;
+
+namespace BookStoreCommonLayer.ResponseModels
+{
+    public class AdminRegistrationResponse
+    {
+        public int AdminID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string UserRole { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
