55e1726d288af039df25532a02a6edcc4c6fe946
vinayakushakola <vinayak.ushakola@gmail.com>
2020-06-23 00:30:14 +0530

    Added: Book Request & Response Models

---
+++ BookStoreCommonLayer/RequestModels/BookRequestModels.cs
@@ -0,0 +1,31 @@
+﻿//
+// Author   : Vinayak Ushakola
+// Date     : 21 June 2020
+// Purpose  : It Contain Book Request Models
+//
+
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreCommonLayer.RequestModels
+{
+    public class BookRequest
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Pages { get; set; }
+    }
+}
---
+++ BookStoreCommonLayer/ResponseModels/BookResponseModels.cs
@@ -0,0 +1,25 @@
+﻿//
+// Author   : Vinayak Ushakola
+// Date     : 21 June 2020
+// Purpose  : It Contain Book Response Models
+//
+
+namespace BookStoreCommonLayer.ResponseModels
+{
+    public class BookResponse
+    {
+        public int BookID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public string Language { get; set; }
+
+        public string Category { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Pages { get; set; }
+    }
+}
