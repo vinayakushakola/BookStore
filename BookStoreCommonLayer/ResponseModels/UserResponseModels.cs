c24c6edde8f8e8acf40a522ddb2f023dc397665c
vinayakushakola <vinayak.ushakola@gmail.com>
2020-06-22 20:06:51 +0530

    Added: User Request & Response Models

---
+++ BookStoreCommonLayer/ResponseModels/UserResponseModels.cs
@@ -0,0 +1,31 @@
+﻿//
+// Author   : Vinayak Ushakola
+// Date     : 21 June 2020
+// Purpose  : It Contain User Response Models
+//
+
+using System;
+
+namespace BookStoreCommonLayer.ResponseModels
+{
+    public class RegistrationResponse
+    {
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string UserRole { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
